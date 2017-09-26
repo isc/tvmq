31f39f789fa296f0f36747482832ce2efb32d365
Ivan Schneider <contact@ivanschneider.fr>
2017-09-26 21:55:33 +0200

    Casting

---
+++ app/assets/javascripts/sender.js
@@ -0,0 +1,76 @@
+var applicationID = 'B48444DF';
+var namespace = 'urn:x-cast:com.google.cast.sample.helloworld';
+var session = null;
+
+if (!chrome.cast || !chrome.cast.isAvailable) {
+  setTimeout(initializeCastApi, 1000);
+}
+
+function initializeCastApi() {
+  var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+  var apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
+  chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+}
+
+function onInitSuccess() {
+  console.log('onInitSuccess')
+}
+
+function onError(message) {
+  console.log('onError: ' + JSON.stringify(message))
+}
+
+function onSuccess(message) {
+  console.log('onSuccess: ' + message)
+}
+
+function onStopAppSuccess() {
+  console.log('onStopAppSuccess')
+}
+
+function sessionListener(e) {
+  console.log('New session ID:' + e.sessionId)
+  session = e;
+  session.addUpdateListener(sessionUpdateListener);
+  session.addMessageListener(namespace, receiverMessage);
+}
+
+function sessionUpdateListener(isAlive) {
+  var message = isAlive ? 'Session Updated' : 'Session Removed';
+  message += ': ' + session.sessionId;
+  console.log(message)
+  if (!isAlive) {
+    session = null;
+  }
+}
+
+function receiverMessage(namespace, message) {
+  console.log('receiverMessage: ' + namespace + ', ' + message)
+}
+
+function receiverListener(e) {
+  if(e === 'available') {
+    console.log('receiver found')
+  }
+  else {
+    console.log('receiver list empty')
+  }
+}
+
+function stopApp() {
+  session.stop(onStopAppSuccess, onError);
+}
+
+function startCasting(message) {
+  if (session != null) {
+    session.sendMessage(namespace, message, onSuccess.bind(this, 'Message sent: ' + message),
+      onError);
+  }
+  else {
+    chrome.cast.requestSession(function(e) {
+        session = e;
+        session.sendMessage(namespace, message, onSuccess.bind(this, 'Message sent: ' +
+          message), onError);
+      }, onError);
+  }
+}
